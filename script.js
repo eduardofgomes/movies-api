aff9d3ab988cd99f61dca04fed70253ae50a7109
eduardofgomes <eduardofranciscogomes01@gmail.com>
2023-02-28 09:53:34 -0300

    Mainly fucntion made

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const API_KEY = "f4ee9eea2def44757865f44c719be46b";
+const image_base_url =  "https://image.tmdb.org/t/p/w1280"
 
 const categories = [
     {
@@ -25,22 +26,44 @@ const categories = [
 
 
 const getMovies = async(path) => {
+    let data = []
     try {
         let url = `https://api.themoviedb.org/3${path}`
         const response = await fetch(url)
-        return await response.json()
-        console.log(response)
+        const responseData = await response.json()
+        
+        data = responseData?.results
+        console.log(data)
     } catch (error) {
-        console.log("Error getMovies: " + error)
+        console.error("Error getMovies: " + error)
     }
 }
 
-function horror () {
-    getMovies(categories.name=horror)
-    var horrorMovies = document.querySelector("#horror").createElement("div")
-    horrorMovies.classList.add("horrorElement")
-    horrorMovies.innerHTML = img
+/*categories.map((category) => {
+    newCard = document.querySelector(".container-app")
+    newCard = document.createElement('div')
+    newCard.setAttribute('id', category.id)
+})*/
+
+const moviesDiv = document.querySelector("#movies")
+
+const renderMovies = async() => {
+    const movies =  await getMovies(`/discover/movie?api_key=${API_KEY}&with_genres=27`)
+    console.log(movies)
+    moviesDiv.innerHTML = movies?.map(movie => renderSingleMovie(movie)).join("")
 }
+
+const renderSingleMovie = (movie) => {
+    return (
+        `
+        <div>
+            <img src="${config.image_base_url + movie?.poster.path}" class="img-fluid">
+        </div>
+        `
+    )
+}
+
+renderMovies()
 
 
 //getMovies(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`)
