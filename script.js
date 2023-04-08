7b7196a3e8719689cb76057820269fe5f8649c3f
eduardofgomes <eduardofranciscogomes01@gmail.com>
2023-04-08 00:53:31 -0300

    release date and rate of movies

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-const API_KEY = "f4ee9eea2def44757865f44c719be46b"
-const image_base_url = "https://image.tmdb.org/t/p/w1280"
-const base_url = "https://api.themoviedb.org/3"
+const api_key = "f4ee9eea2def44757865f44c719be46b"
+const imageBaseUrl = "https://image.tmdb.org/t/p/w1280"
+const baseUrl = "https://api.themoviedb.org/3"
 
 const categories = [
     {
         name: "horror",
         title: "horror",
-        path: `/discover/movie?api_key=${API_KEY}&with_genres=27`
+        path: `/discover/movie?api_key=${api_key}&with_genres=27`
     },
     {
         name: "war",
         title: "war",
-        path: `/discover/movie?api_key=${API_KEY}&with_genres=10752`
+        path: `/discover/movie?api_key=${api_key}&with_genres=10752`
     },
     {
         name: "animation",
         title: "animation",
-        path: `/discover/movie?api_key=${API_KEY}&with_genres=16`
+        path: `/discover/movie?api_key=${api_key}&with_genres=16`
     },
     {
         name: "action",
         title: "action",
-        path: `/discover/movie?api_key=${API_KEY}&with_genres=28`
+        path: `/discover/movie?api_key=${api_key}&with_genres=28`
     }
 ]
 
 const getMovies = async(path) => {
     try {
-        let url = `${base_url}${path}`
+        let url = `${baseUrl}${path}`
         const response = await fetch(url)
         const responseData = await response.json()
         data = responseData?.results
@@ -39,7 +39,7 @@ const getMovies = async(path) => {
 
 /*const getTrailer = async (id) => {
     try {
-        let url = `${base_url}/movie/${id}/videos?api_key=${API_KEY}&language=en-US`
+        let url = `${baseUrl}/movie/${id}/videos?api_key=${api_key}&language=en-US`
         const response = await fetch(url)
         const responseData = await response.json()
         dataTrailer = responseData?.results
@@ -53,17 +53,20 @@ const renderSingleMovie = (movies) => {
     console.log(movies)
     return (
         `   
-        <img src="${image_base_url + movies?.poster_path}" class="poster" data-modal="modal${movies?.id}" />
+        <img src="${imageBaseUrl + movies?.poster_path}" class="poster" data-modal="modal${movies?.id}" />
 
         <div id="modal${movies?.id}" class="modal"> 
             <div class="modal-content">
                 <div class="modal-close">
-                        <span class="close">&times;</span>
+                    <span class="close">&times;</span>
                 </div>
                 <div class="modal-information">
-                    <img src="${image_base_url + movies?.backdrop_path}" class="image-modal" />
-                    <h3 class="title">${movies?.title}</h3>
-                    <p class="title">${movies?.overview}</p>
+                    <img src="${imageBaseUrl + movies?.backdrop_path}" class="image-modal text" alt="Image not found"/>
+                    <h2 class="text">${movies?.title}</h2>
+                    <p class="text overview">${movies?.overview}</p>
+                    <p class="info-movie text">${movies?.release_date ? movies?.release_date.split('-')[0] : "Not Released"}
+                    rate: ${movies?.vote_average ? movies?.vote_average : "Not rated"}
+                    </p>
                 </div>
             </div>
         </div>
@@ -93,7 +96,7 @@ function showMovie() {
     })
 }
 
-setTimeout(() => { //use async and await in this function
+setTimeout(() => {
     var button = document.querySelectorAll(".poster")
     button.forEach(function(btn) {
         btn.onclick = () => {
