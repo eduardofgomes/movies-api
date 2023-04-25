201f5096ddae110b9156a5f55e59eac6ed895e14
eduardofgomes <eduardofranciscogomes01@gmail.com>
2023-04-25 18:04:04 -0300

    trailer again

--- script.js
+++ script.js
@@ -87,14 +87,13 @@ function showMovie() {
                 moviesDiv.setAttribute("class", "moviesGenre")
                 document.querySelector("#movies").appendChild(moviesDiv);
                 const movies = await getMovies(category.path)
-                for await (const video of movies) {
-                    trailer = getTrailer(video?.id).then(result => {return result})
-                    console.log(trailer)
-                }
                 //const trailer = await getTrailer(movies[0].id)
-                //console.log(trailer)
                 moviesDiv.innerHTML = movies?.map((movie) => {
-                    //trailer = getTrailer(movie.id).then(result => {return result})
+                    movies.map(async i => {
+                        const trailer = await getTrailer(i?.id)
+                        console.log(trailer)
+                    })
+                    
                     //console.log(trailer)
                     return renderSingleMovie(movie, trailer)
                 }).join("")
